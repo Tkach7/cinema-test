c6e3538530fac1a3764e5fca55aa2af1cdab3828
Daniil Tkach <datkach@exadel.com>
2019-05-12 20:39:57 +0500

    added local for moment;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as moment from "moment";
+import "moment/locale/es";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'cinema';
+  title = "cinema";
+
+  constructor() {
+    moment.locale("ru");
+  }
 }
--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,1 +1,1 @@
-export const FORMAT_DAY = "DD-MM-YYYY";
+export const FORMAT_DAY = 'D MMMM, YYYY';
